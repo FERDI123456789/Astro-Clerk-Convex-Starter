0ca7968db92702fcff696bd45308d586de4a47b8
FERDI123456789 <ferdi0909lol@pm.me>
2026-02-08 02:03:09 +0100

    Added the astro vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,26 +1,22 @@
 // @ts-check
 import { defineConfig } from "astro/config";
 
-import node from "@astrojs/node";
-
+import vercel from "@astrojs/vercel";
 import react from "@astrojs/react";
-
 import clerk from "@clerk/astro";
-
 import tailwindcss from "@tailwindcss/vite";
 
-// https://astro.build/config
 export default defineConfig({
-  adapter: node({
-    mode: "standalone",
-  }),
-
   integrations: [react(), clerk()],
 
   vite: {
     // @ts-ignore
     plugins: [tailwindcss()],
   },
-  server: { port: 7878 },
+
+  server: {
+    port: 7878,
+  },
+  adapter: vercel(),
   output: "server",
 });
